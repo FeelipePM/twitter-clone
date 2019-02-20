b5e500058c93e1ec4af1ec5be6c98a2e8520f6ac
Felipe Machado <feelipe@email.com>
2019-02-20 14:01:41 -0300

    Exigibindo tweet de quem seguimos, Motando painel de tweets e seguidores

--- procurar_pessoas.php
+++ procurar_pessoas.php
@@ -6,6 +6,41 @@
 		header('Location: index.php?erro=1');
 	}
 
+	require_once('db.class.php');
+
+	$objectDb = new db();
+	$link = $objectDb->conecta_mysql();
+	
+	$id_usuario = $_SESSION['id_usuario'];
+
+	// quantidades de tweets
+	$sql = " SELECT COUNT(*) AS qtde_tweets FROM tweet WHERE id_usuario = $id_usuario ";
+
+	$resultado_id = mysqli_query($link, $sql);
+
+	$qtde_tweets = 0;
+
+	if($resultado_id){
+		$registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
+		$qtde_tweets = $registro['qtde_tweets'];
+	} else {
+		echo 'Erro ao executar a query';
+	}
+
+		//quantidades de seguidores
+	$sql = " SELECT COUNT(*) AS qtde_seguidores FROM usuarios_seguidores WHERE seguindo_id_usuario = $id_usuario ";
+
+	$resultado_id = mysqli_query($link, $sql);
+
+	$qtde_seguidores = 0;
+
+	if($resultado_id){
+		$registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
+		$qtde_seguidores = $registro['qtde_seguidores'];
+	} else {
+		echo 'Erro ao executar a query';
+	}
+
 ?>
 
 <!DOCTYPE HTML>
@@ -57,10 +92,10 @@
 
 							 <hr />
 								<div class="col-md-6">
-									Tweets <br /> 1
+									Tweets <br /> <?= $qtde_tweets ?>
 								</div>
 								<div class="col-md-6">
-									Seguidores <br /> 1
+									Seguidores <br /> <?= $qtde_seguidores ?>
 								</div>
 						</div>
 					</div>
