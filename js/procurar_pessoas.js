2fd973bb30ee35952172b411457830ebaf22ff6d
Felipe Machado <feelipe@email.com>
2019-02-19 21:21:00 -0300

    Controlando a exibição do botão seguir e deixar de seguir pessoas

--- js/procurar_pessoas.js
+++ js/procurar_pessoas.js
@@ -1,33 +1,40 @@
-$(document).ready(function() {
-  $("#btn_procurar_pessoa").click(function() {
+$(document).ready(function () {
+  $("#btn_procurar_pessoa").click(function () {
     if ($("#nome_pessoa").val().length > 0) {
       $.ajax({
         url: "get_pessoas.php",
         method: "POST",
         data: $("#form_procurar_pessoas").serialize(),
-        success: function(data) {
+        success: function (data) {
           $("#pessoas").html(data);
 
-          $(".btn_seguir").click(function() {
+          $(".btn_seguir").click(function () {
             var id_usuario = $(this).data("id_usuario");
+
+            $("#btn_seguir_" + id_usuario).hide();
+            $("#btn_deixar_seguir_" + id_usuario).show();
 
             $.ajax({
               url: "seguir.php",
               method: "POST",
               data: { seguir_id_usuario: id_usuario },
-              success: function(data) {
+              success: function (data) {
                 console.log("Requisição Efetuada com Sucesso!");
               }
             });
           });
-          $(".btn_deixar_seguir").click(function() {
+
+          $(".btn_deixar_seguir").click(function () {
             var id_usuario = $(this).data("id_usuario");
+
+            $("#btn_seguir_" + id_usuario).show();
+            $("#btn_deixar_seguir_" + id_usuario).hide();
 
             $.ajax({
               url: "deixar_seguir.php",
               method: "POST",
               data: { deixar_seguir_id_usuario: id_usuario },
-              success: function(data) {
+              success: function (data) {
                 console.log("Requisição Removida com Sucesso!");
               }
             });
